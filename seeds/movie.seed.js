38b0822be5fb442db77fc3ef7859668f2169308c
Johanna Angot <63612183+jooohke@users.noreply.github.com>
2021-07-22 16:03:14 +0200

    crash

---
+++ seeds/movie.seed.js
@@ -0,0 +1,11 @@
+// const mongoose = require("mongoose");
+// const Movie = require("../models/Movie.model.js");
+
+// mongoose
+// .connect("mongodb://localhost/lab-express-cinema")
+// .then(async () => {
+//     await Movie.deleteMany();
+// })
+// .catch((error) => {
+//     console.log(error)
+// })
