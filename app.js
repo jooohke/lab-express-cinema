5dfbd29573b9c78948f88937810caab55c77a67e
Johanna Angot <63612183+jooohke@users.noreply.github.com>
2021-07-23 09:04:57 +0200

    iteration 4 almost done - still some problems with the values as arrays

--- app.js
+++ app.js
@@ -32,4 +32,7 @@ app.use('/', index);
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
 
+import 'bootstrap';
+
+
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Movie = require("../models/Movie.model")
+const moongoose = require("mongoose")
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index', {css:['main.css', 'home.css']}));
@@ -21,22 +22,23 @@ router.get('/movies', (req,res) => {
 })
 
 router.get('/movies/:id', (req,res) => {
-    const isValid = mongoose.isValidObjectId(req.params.id);
+    // const isValid = mongoose.isValidObjectId(req.params.id);
 
-    if(isValid) {
+    // if(isValid) {
         Movie.findById(req.params.id)
         .then((movieDocument) => {
-            console.log.log(movieDocument);
+            console.log("here's the movieDocument" + movieDocument);
             res.render("oneMovie.hbs", {
-                movie: movieDocument
+                movie: movieDocument,
+                css:['style.css']
             });
         })
         .catch((error) => {
             console.log(error)
         })
-    } else {
-        res.render("not-found.hbs")
-    }
+    // } else {
+    //     res.render("not-found.hbs")
+    // }
 })
 
 module.exports = router;
